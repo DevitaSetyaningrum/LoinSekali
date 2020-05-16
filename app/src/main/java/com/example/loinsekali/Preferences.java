6880be8e9071e01ba06737d01cdcbe5585064c21
devita <ningrum.deviita@gmail.com>
2020-05-16 11:25:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/loinsekali/Preferences.java
@@ -0,0 +1,82 @@
+package com.example.loinsekali;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class Preferences {
+
+
+    static final String KEY_USER_TEREGISTER = "user", KEY_PASS_TEREGISTER = "pass";
+    static final String KEY_USERNAME_SEDANG_LOGIN = "Username_logged_in";
+    static final String KEY_STATUS_SEDANG_LOGIN = "Status_logged_in";
+/*
+    public static String getKeyUserTeregister(Context context) {
+        return KEY_USER_TEREGISTER;
+    }
+
+    public static String getKeyPassTeregister(Context context) {
+        return KEY_PASS_TEREGISTER;
+    }
+
+    public static String getKeyUsernameSedangLogin(Context context) {
+        return KEY_USERNAME_SEDANG_LOGIN;
+    }
+
+    public static String getKeyStatusSedangLogin(Context context) {
+        return KEY_STATUS_SEDANG_LOGIN;
+    }
+
+*/
+    private static SharedPreferences getSharedPreference(Context context){
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+
+    public static void setRegisteredUser(Context context, String username){
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.putString(KEY_USER_TEREGISTER, username);
+        editor.apply();
+    }
+
+    public static String getRegisteredUser(Context context){
+        return getSharedPreference(context).getString(KEY_USER_TEREGISTER,"");
+    }
+
+    public static void setRegisteredPass(Context context, String password){
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.putString(KEY_PASS_TEREGISTER, password);
+        editor.apply();
+    }
+
+    public static String getRegisteredPass(Context context){
+        return getSharedPreference(context).getString(KEY_PASS_TEREGISTER,"");
+    }
+
+    public static void setLoggedInUser(Context context, String username){
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.putString(KEY_USERNAME_SEDANG_LOGIN, username);
+        editor.apply();
+    }
+
+    public static String getLoggedInUser(Context context){
+        return getSharedPreference(context).getString(KEY_USERNAME_SEDANG_LOGIN,"");
+    }
+
+    public static void setLoggedInStatus(Context context, boolean status){
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.putBoolean(KEY_STATUS_SEDANG_LOGIN,status);
+        editor.apply();
+    }
+
+    public static boolean getLoggedInStatus(Context context){
+        return getSharedPreference(context).getBoolean(KEY_STATUS_SEDANG_LOGIN,false);
+    }
+
+    public static void clearLoggedInUser (Context context){
+        SharedPreferences.Editor editor = getSharedPreference(context).edit();
+        editor.remove(KEY_USERNAME_SEDANG_LOGIN);
+        editor.remove(KEY_STATUS_SEDANG_LOGIN);
+        editor.apply();
+    }
+
+}
